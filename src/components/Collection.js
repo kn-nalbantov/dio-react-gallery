30cc6939a47ea6cf7206b9dcc81ff2443be811fb
kn-nalbantov <kn.nalbantov@gmail.com>
2022-02-07 21:43:33 +0200

    imported svgs and images in bulk

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -2,17 +2,19 @@ import React from 'react';
 import kaos from '../icons/kaos.svg';
 import lamatta from '../icons/lamatta.svg';
 import rr from '../icons/robertrudger.svg';
+import pic0 from '../pictures/1.JPG';
 
 export default function Collection(prop) {
+  const svgs = {kaos, lamatta, rr};
   return (
     <div tabIndex="-1" className="collection">
-      <img src={prop.name} alt="La Matta logo" class="logo" />
+      <img src={svgs[prop.name]} alt="La Matta logo" className="logo" />
             <p>Бъди различен! Ла Мата взима прекаленото и му инжектира стил. <a href="https://area98.it/collections/lamatta" target="_blank">Научете повече.</a></p>
             <button>⬇</button>
-            <div class="fullimg">
-                <img src="" alt="picture of a La Matta" class="displayed" />
+            <div className="fullimg">
+                <img src={pic0} alt="picture of a La Matta" className="displayed" />
             </div>
-            <div class="thumb-bar"></div>
+            <div className="thumb-bar"></div>
     </div>
   )
 }
