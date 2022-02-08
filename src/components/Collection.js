d00d1fcb611b66ef4277e5b28ff0662f20772996
kn-nalbantov <kn.nalbantov@gmail.com>
2022-02-08 21:45:01 +0200

    added image thumbnail

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -3,16 +3,19 @@ import kaos from '../icons/kaos.svg';
 import lamatta from '../icons/lamatta.svg';
 import rr from '../icons/robertrudger.svg';
 import pic0 from '../pictures/1.JPG';
+import images from '../images.js';
 
 export default function Collection(prop) {
   const svgs = {kaos, lamatta, rr};
+  const imgs = {kaos: images['6.JPG'], lamatta: images['0.JPG'], rr: images['12.JPG']}
+  console.log(images)
   return (
     <div tabIndex="-1" className="collection">
       <img src={svgs[prop.name]} alt="La Matta logo" className="logo" />
             <p>Бъди различен! Ла Мата взима прекаленото и му инжектира стил. <a href="https://area98.it/collections/lamatta" target="_blank">Научете повече.</a></p>
             <button>⬇</button>
             <div className="fullimg">
-                <img src={pic0} alt="picture of a La Matta" className="displayed" />
+                <img src={imgs[prop.name]} alt="picture of a La Matta" className="displayed" />
             </div>
             <div className="thumb-bar"></div>
     </div>
