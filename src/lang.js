9299b9434b9ba8adbe08176d98a201c192b16d69
kn-nalbantov <kn.nalbantov@gmail.com>
2022-02-14 22:18:01 +0200

    fixed duplicate translation

--- src/lang.js
+++ src/lang.js
@@ -2,7 +2,7 @@ const eng = {
   title: 'Dio gallery',
   lamatta: 'Dare to be different! La Matta takes excess and injected it with style.',
   kaos: 'Never banal. Kaos - colourful, original and playful.',
-  rr: 'For the remarkable man who is a cut above the rest.',
+  robertrudger: 'For the remarkable man who is a cut above the rest.',
   learn: 'Learn more.',
 };
 
@@ -10,16 +10,18 @@ const bg = {
   title: 'Галерия Дио',
   lamatta: 'Бъди различен! Ла Мата взима прекаленото и му инжектира стил.',
   kaos: 'Никога банален. Каос - цветен, оригинален и игрив.',
-  rr: 'Роберт Рюдгер - за отличителния мъж, който взима преднина пред останалите.',
+  robertrudger: 'Роберт Рюдгер - за отличителния мъж, който взима преднина пред останалите.',
   learn: 'Научете повече.',
 };
 
 const it = {
   title: 'Dio gallery',
   lamatta: 'LA MATTA L’INTERPRETAZIONE DELL’ECCESSO “CON STILE”.',
-  kaos: 'LA MATTA L’INTERPRETAZIONE DELL’ECCESSO “CON STILE”.',
-  rr: "Per l'uomo straordinario che è una spanna sopra gli altri.",
+  kaos: 'Mai banale. Kaos - coloratissimo, originale e scherzoso.',
+  robertrudger: "Per l'uomo straordinario che è una spanna sopra gli altri.",
   learn: 'Per saperne di più.',
 };
 
-export { eng, bg, it };
+const lang = { eng, bg, it }
+
+export { lang };
