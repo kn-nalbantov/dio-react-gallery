46e908c398c02b7f315fea223f5ed9d61b071a9a
kn-nalbantov <kn.nalbantov@gmail.com>
2022-02-14 22:19:47 +0200

    changes to prop.lang in Collection.js

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import kaos from '../icons/kaos.svg';
 import lamatta from '../icons/lamatta.svg';
-import rr from '../icons/robertrudger.svg';
+import robertrudger from '../icons/robertrudger.svg';
 import { images } from '../images.js';
-import {eng, bg, it} from '../lang.js';
+import { lang } from '../lang.js';
 
 export default function Collection(prop) {
-  const svgs = {kaos, lamatta, rr};
-  const imgs = {kaos: images['6.JPG'], lamatta: images['0.JPG'], rr: images['12.JPG']}
-  const langs = { eng, bg, it };
+  const svgs = {kaos, lamatta, robertrudger};
+  const imgs = {kaos: images['6.JPG'], lamatta: images['0.JPG'], robertrudger: images['12.JPG']}
+  console.log(prop.lang)
   return (
     <div tabIndex="-1" className="collection">
       <img src={svgs[prop.name]} alt="La Matta logo" className="logo" />
-            <p>{langs[prop.lang][prop.name]} <a href="https://area98.it/collections/lamatta" target="_blank" rel="noreferrer">Научете повече.</a></p>
+            <p>{lang[prop.lang][prop.name]} <a href={"https://area98.it/collections/" + prop.name} target="_blank" rel="noreferrer">{lang[prop.lang]['learn']}</a></p>
             <button>⬇</button>
             <div className="fullimg">
-                <img src={imgs[prop.name]} alt="La Matta" className="displayed" />
+                <img src={imgs[prop.name]} alt={prop.name} className="displayed" />
             </div>
             <div className="thumb-bar"></div>
     </div>
