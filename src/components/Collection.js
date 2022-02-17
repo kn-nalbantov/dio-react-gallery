f1f0a1ec324818dd4c5ae62969402a80d7ba0905
kn-nalbantov <kn.nalbantov@gmail.com>
2022-02-17 22:45:27 +0200

    added collections to thumb-bar and added onclick event

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -2,22 +2,34 @@ import React from 'react';
 import kaos from '../icons/kaos.svg';
 import lamatta from '../icons/lamatta.svg';
 import robertrudger from '../icons/robertrudger.svg';
-import { images } from '../images.js';
+import { imgs } from '../images.js';
 import { lang } from '../lang.js';
 
 export default function Collection(prop) {
-  const svgs = {kaos, lamatta, robertrudger};
-  const imgs = {kaos: images['6.JPG'], lamatta: images['0.JPG'], robertrudger: images['12.JPG']}
-  console.log(prop.lang)
+  const svgs = { kaos, lamatta, robertrudger };
+
+  function onClick(e) {
+    // document.querySelectorAll('.thumb-bar').forEach(x => x.style.display = 'block');
+    //change display of e.target thumb-bar pics
+  }
   return (
-    <div tabIndex="-1" className="collection">
-      <img src={svgs[prop.name]} alt="La Matta logo" className="logo" />
-            <p>{lang[prop.lang][prop.name]} <a href={"https://area98.it/collections/" + prop.name} target="_blank" rel="noreferrer">{lang[prop.lang]['learn']}</a></p>
-            <button>⬇</button>
-            <div className="fullimg">
-                <img src={imgs[prop.name]} alt={prop.name} className="displayed" />
-            </div>
-            <div className="thumb-bar"></div>
+    <div tabIndex='-1' className='collection'>
+      <img src={svgs[prop.name]} alt='La Matta logo' className='logo' />
+      <p>
+        {lang[prop.lang][prop.name]}{' '}
+        <a href={'https://area98.it/collections/' + prop.name} target='_blank' rel='noreferrer'>
+          {lang[prop.lang]['learn']}
+        </a>
+      </p>
+      <button onClick={onClick}>⬇</button>
+      <div className='fullimg'>
+        <img src={imgs[prop.name][0]} alt={prop.name} className='displayed' />
+      </div>
+      <div>
+        {imgs[prop.name].map((x) => (
+          <img src={x} key={x + 1} className='thumb-bar' />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
